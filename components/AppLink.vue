1d72730c6c8558505c1c7471d58dccea8c18e1db
Marc Backes <hello@marc.dev>
2022-07-11 19:11:06 +0200

    added glow on links in dark mode

--- components/AppLink.vue
+++ components/AppLink.vue
@@ -5,7 +5,7 @@ const { to, muted } = defineProps({
     muted: { type: Boolean, default: false },
 })
 
-const defaultClasses = ['font-bold', 'hover:underline', 'mx-1']
+const defaultClasses = ['font-bold', 'hover:underline', 'hover:dark:no-underline', 'mx-1', 'background-shadow']
 const { classes } = useClass(defaultClasses, {
     'text-black dark:text-white': !muted,
     'hover:text-black hover:dark:text-white': muted,
@@ -13,10 +13,19 @@ const { classes } = useClass(defaultClasses, {
 </script>
 
 <template>
-    <NuxtLink :to="to" :class="classes">
-        {{}}
+    <NuxtLink
+        :to="to"
+        :class="classes">
         <slot />
     </NuxtLink>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.dark {
+    .background-shadow {
+        &:hover {
+            text-shadow: 0px 0px 10px white;
+        }
+    }
+}
+</style>
