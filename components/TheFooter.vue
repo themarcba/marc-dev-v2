8ace777c3f9afa8f91ed53e1ea1bd6a523903ca7
Marc Backes <hello@marc.dev>
2022-07-11 19:16:15 +0200

    fixed footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,15 +1,29 @@
 <template>
-    <footer class="flex align-center justify-center p-2">
+    <footer
+        class="text-center justify-center p-2">
         Made by with
-        <AppLink to="https://v3.nuxtjs.org/" :muted="true">Nuxt 3</AppLink>. Hosted on
-        <AppLink to="https://netlify.com/" :muted="true">Netlify</AppLink>. Code is open
+        <AppLink
+            to="https://v3.nuxtjs.org/"
+            :muted="true">
+            Nuxt 3
+        </AppLink>.
+        Hosted on
+        <AppLink
+            to="https://netlify.com/"
+            :muted="true">
+            Netlify
+        </AppLink>.
+        Code is open
         on
-        <AppLink to="https://github.com/themarcba/marc-dev-v2/" :muted="true"
-            >GitHub</AppLink
-        >.
+        <AppLink
+            to="https://github.com/themarcba/marc-dev-v2/"
+            :muted="true">
+            GitHub
+        </AppLink>.
     </footer>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
